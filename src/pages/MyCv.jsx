801334b8cbd78108dd65bd3a9d1ee42a34897257
Loris <loulounav78@gmail.com>
2024-11-29 20:02:42 +0100

    update

--- src/pages/MyCv.jsx
+++ src/pages/MyCv.jsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState, useContext } from 'react';
-import { useParams, useNavigate, Link } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { Field, Form, Formik, FieldArray } from 'formik';
 import { AuthContext } from '../context/AuthContext.jsx';
 import '../css/MyCv.css';
 import * as Yup from 'yup';
 
 function MyCv() {
-    const { user, updateUser , token} = useContext(AuthContext);
-    
+    const { user, token } = useContext(AuthContext);
     const navigate = useNavigate();
     const [cv, setCv] = useState(null);
     const [cvNotFound, setCvNotFound] = useState(false);
@@ -15,18 +14,16 @@ function MyCv() {
     useEffect(() => {
         const fetchCv = async () => {
             try {
-                const response = await fetch(`https://efrei-api-rest-project-g2.onrender.com/api/cv/user/${user.id}`, {
-                    method: 'GET',
-                    headers: {
-                        Authorization: `Bearer ${token}`
-                    },
-                });
-                if (!response.ok) {
-                    if (response.status === 404) {
-                        setCvNotFound(true); // CV non trouvé
-                    } else {
-                        throw new Error(`HTTP error! Status: ${response.status}`);
+                const response = await fetch(
+                    `https://efrei-api-rest-project-g2.onrender.com/api/cv/user/${user.id}`,
+                    {
+                        method: 'GET',
+                        headers: { Authorization: `Bearer ${token}` },
                     }
+                );
+                if (!response.ok) {
+                    if (response.status === 404) setCvNotFound(true);
+                    else throw new Error(`HTTP error! Status: ${response.status}`);
                 } else {
                     const data = await response.json();
                     setCv(data);
@@ -38,21 +35,22 @@ function MyCv() {
         };
 
         fetchCv();
-    }, [user.id]);
+    }, [user.id, token]);
+
 
     const deleteCV = async () => {
-        if (window.confirm('Êtes-vous sûr de vouloir supprimer ce CV ? Cette action est irréversible.')) {
+        if (window.confirm('Êtes-vous sûr de vouloir supprimer ce CV ?')) {
             try {
-                const response = await fetch(`https://efrei-api-rest-project-g2.onrender.com/api/cv/${user.id}`, {
-                    method: 'DELETE',
-                });
-
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-
+                const response = await fetch(
+                    `https://efrei-api-rest-project-g2.onrender.com/api/cv/${cv[0]._id}`,
+                    {
+                        method: 'DELETE',
+                        headers: { Authorization: `Bearer ${token}` },
+                    }
+                );
+                if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
                 alert('CV supprimé avec succès !');
-                navigate('/'); // Redirection après suppression
+                navigate('/');
             } catch (error) {
                 console.error('Failed to delete CV:', error);
                 alert('Échec de la suppression du CV.');
@@ -66,10 +64,10 @@ function MyCv() {
                 <h2>Aucun CV trouvé</h2>
                 <p>
                     Vous n'avez pas encore créé de CV. Cliquez{' '}
-                    <Link to="/create-cv" className="btn btn-link">
-                        ICI
+                    <Link to="/create-cv" className="btn-link">
+                        ici
                     </Link>{' '}
-                    pour créer un nouveau CV.
+                    pour en créer un.
                 </p>
             </div>
         );
@@ -78,16 +76,12 @@ function MyCv() {
     if (!cv) {
         return <p>Chargement...</p>;
     }
-
+    
     const initialValues = {
-        personalInfo: cv.personalInfo || {
-            firstName: '',
-            lastName: '',
-            description: '',
-        },
-        education: cv.education || [],
-        experience: cv.experience || [],
-        isVisible: cv.isVisible || false,
+        personalInfo: cv[0].personalInfo || { firstName: '', lastName: '', description: '' },
+        education: cv[0].education || [],
+        experience: cv[0].experience || [],
+        isVisible: cv[0].isVisible || false,
     };
 
     const validationSchema = Yup.object({
@@ -108,10 +102,10 @@ function MyCv() {
         ),
         experience: Yup.array().of(
             Yup.object({
-                jobTitle: Yup.string().required('Titre de poste requis'),
+                jobTitle: Yup.string().required('Poste requis'),
                 company: Yup.string().required('Entreprise requise'),
                 years: Yup.number()
-                    .required('Nombre d’années requis')
+                    .required('Années requises')
                     .min(0, 'Années invalides'),
             })
         ),
@@ -119,18 +113,18 @@ function MyCv() {
 
     const handleSubmit = async (values) => {
         try {
-            const response = await fetch(`https://efrei-api-rest-project-g2.onrender.com/api/cv/${id}`, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(values),
-            });
-
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-
+            const response = await fetch(
+                `https://efrei-api-rest-project-g2.onrender.com/api/cv/${cv[0]._id}`,
+                {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${token}`,
+                    },
+                    body: JSON.stringify(values),
+                }
+            );
+            if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
             alert('CV mis à jour avec succès !');
         } catch (error) {
             console.error('Failed to update CV:', error);
@@ -139,8 +133,11 @@ function MyCv() {
     };
 
     return (
-        <div className="view-edit-cv-container">
-            <h1>Afficher et Modifier le CV</h1>
+        <div className="my-cv-container">
+            <button onClick={navigate(`/CvViewer/${cv[0]._id}`)} className="mycv-button mycv-button-navigate">
+                Voir mon CV formater et les commentaires recu
+             </button>
+            <h1>Gérer mon CV</h1>
             <Formik
                 initialValues={initialValues}
                 validationSchema={validationSchema}
@@ -148,71 +145,56 @@ function MyCv() {
             >
                 {({ values, handleChange }) => (
                     <Form>
-                        {/* Informations personnelles */}
-                        <div className="form-section">
-                            <h2>Informations personnelles</h2>
-                            <label>Prénom :</label>
-                            <Field
-                                className="form-control"
-                                type="text"
-                                name="personalInfo.firstName"
-                                placeholder="Prénom"
-                            />
-                            <label>Nom :</label>
-                            <Field
-                                className="form-control"
-                                type="text"
-                                name="personalInfo.lastName"
-                                placeholder="Nom"
-                            />
-                            <label>Description :</label>
-                            <Field
-                                as="textarea"
-                                className="form-control"
-                                name="personalInfo.description"
-                                placeholder="Décrivez-vous"
-                            />
+                        <div>
+                            <h2 className='top_space'>Informations personnelles</h2>
+                            <FieldArray name="personalInfo">
+                                {({ remove, push }) => (
+                                    <>
+                                        <Field name={`personalInfo.firstName`} placeholder="firstName" />
+                                        <Field name={`personalInfo.lastName`} placeholder="lastName" />
+                                        <Field name={`personalInfo.description`} placeholder="description" />
+                                        <button
+                                            type="button"
+                                            className="mycv-button mycv-button-remove"
+                                            onClick={() => remove(index)}
+                                            >
+                                            Retirer
+                                        </button>
+                                            
+                                        <button
+                                            type="button"
+                                            className="mycv-button mycv-button-add"
+                                            onClick={() => push({ firstName: '', lastName: '', description: '' })}
+                                        >
+                                            Ajouter une personalInfo
+                                        </button>
+                                    </>
+                                )}
+                            </FieldArray>
                         </div>
 
-                        {/* Éducation */}
-                        <div className="form-section">
-                            <h2>Éducation</h2>
+                        <div>
+                            <h2 className='top_space'>Éducation</h2>
                             <FieldArray name="education">
                                 {({ remove, push }) => (
                                     <>
-                                        {values.education.map((edu, index) => (
-                                            <div key={index} className="nested-form-group">
-                                                <label>Diplôme :</label>
-                                                <Field
-                                                    className="form-control"
-                                                    name={`education.${index}.degree`}
-                                                    placeholder="Diplôme"
-                                                />
-                                                <label>Établissement :</label>
-                                                <Field
-                                                    className="form-control"
-                                                    name={`education.${index}.institution`}
-                                                    placeholder="Établissement"
-                                                />
-                                                <label>Année :</label>
-                                                <Field
-                                                    className="form-control"
-                                                    type="number"
-                                                    name={`education.${index}.year`}
-                                                    placeholder="Année"
-                                                />
+                                        {values.education.map((_, index) => (
+                                            <div key={index}>
+                                                <Field name={`education.${index}.degree`} placeholder="Diplôme" />
+                                                <Field name={`education.${index}.institution`} placeholder="Établissement" />
+                                                <Field name={`education.${index}.year`} placeholder="Année" type="number" />
                                                 <button
                                                     type="button"
-                                                    className="btn btn-danger button-cv"
+                                                    className="mycv-button mycv-button-remove"
                                                     onClick={() => remove(index)}
                                                 >
-                                                    Supprimer
+                                                    Retirer
                                                 </button>
                                             </div>
                                         ))}
                                         <button
                                             type="button"
-                                            className="btn btn-secondary button-cv"
+                                            className="mycv-button mycv-button-add"
                                             onClick={() => push({ degree: '', institution: '', year: '' })}
                                         >
                                             Ajouter une éducation
@@ -222,45 +204,28 @@ function MyCv() {
                             </FieldArray>
                         </div>
 
-                        {/* Expérience */}
-                        <div className="form-section">
-                            <h2>Expérience</h2>
+                        <div>
+                            <h2 className='top_space'>Expérience</h2>
                             <FieldArray name="experience">
                                 {({ remove, push }) => (
                                     <>
-                                        {values.experience.map((exp, index) => (
-                                            <div key={index} className="nested-form-group">
-                                                <label>Poste :</label>
-                                                <Field
-                                                    className="form-control"
-                                                    name={`experience.${index}.jobTitle`}
-                                                    placeholder="Poste"
-                                                />
-                                                <label>Entreprise :</label>
-                                                <Field
-                                                    className="form-control"
-                                                    name={`experience.${index}.company`}
-                                                    placeholder="Entreprise"
-                                                />
-                                                <label>Années :</label>
-                                                <Field
-                                                    className="form-control"
-                                                    type="number"
-                                                    name={`experience.${index}.years`}
-                                                    placeholder="Années"
-                                                />
+                                        {values.experience.map((_, index) => (
+                                            <div key={index}>
+                                                <Field name={`experience.${index}.jobTitle`} placeholder="Poste" />
+                                                <Field name={`experience.${index}.company`} placeholder="Entreprise" />
+                                                <Field name={`experience.${index}.years`} placeholder="Années" type="number" />
                                                 <button
                                                     type="button"
-                                                    className="btn btn-danger button-cv"
+                                                    className="mycv-button mycv-button-remove"
                                                     onClick={() => remove(index)}
                                                 >
-                                                    Supprimer
+                                                    Retirer
                                                 </button>
                                             </div>
                                         ))}
                                         <button
                                             type="button"
-                                            className="btn btn-secondary button-cv"
+                                            className="mycv-button mycv-button-add"
                                             onClick={() => push({ jobTitle: '', company: '', years: '' })}
                                         >
                                             Ajouter une expérience
@@ -270,30 +235,24 @@ function MyCv() {
                             </FieldArray>
                         </div>
 
-                        {/* Visibilité */}
-                        <div className="form-section">
+                        <div>
                             <label>
-                                <Field
-                                    type="checkbox"
-                                    name="isVisible"
-                                    checked={values.isVisible}
-                                    onChange={handleChange}
-                                />
+                                <Field type="checkbox" name="isVisible" onChange={handleChange} />
                                 Rendre le CV public
                             </label>
                         </div>
 
-                        <button className="btn btn-primary mt-3 button-cv" type="submit">
+                        <button type="submit" className="mycv-button mycv-button-save">
                             Enregistrer
+                        </button>
+                        <button onClick={deleteCV} className="mycv-button mycv-button-delete">
+                            Supprimer mon CV
                         </button>
                     </Form>
                 )}
             </Formik>
-            <button className="btn btn-danger mt-3 button-cv" onClick={deleteCV}>
-                Supprimer ce CV
-            </button>
         </div>
-    );
-}
 
+            );
+        }
 export default MyCv;
